1933608084b7cb2bd1675b4cf024891330735a04
John Leonardo <jdleonardo5@icloud.com>
2018-07-17 23:52:49 -0700

    contract source

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] deployedCampaigns;
+
+    function CampaignFactory(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+
+}
+
+contract Campaign {
+
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint public minimumAmount;
+    uint public approversCount;
+    Request[] public requests;
+    mapping(address => bool) public approvers;
+
+    modifier restricted {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Campaign(uint minimum, address sender) public {
+        manager = sender;
+        minimumAmount = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumAmount);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage req = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!req.approvals[msg.sender]);
+
+        req.approvals[msg.sender] = true;
+        req.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public {
+        Request storage req = requests[index];
+
+        require(!req.complete);
+        require(req.approvalCount > (approversCount / 2));
+
+        req.complete = true;
+        req.recipient.transfer(req.value);
+    }
+}
